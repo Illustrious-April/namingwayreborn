30326bc2954d6331c647a2ac6bc71300f45414a6
Illustrious-April <123636224+Illustrious-April@users.noreply.github.com>
2025-03-05 17:01:53 +1000

    Update Plugin.cs
    Renaming

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -4,9 +4,9 @@ using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using NamingwayReborn.Windows;
 
-namespace SamplePlugin;
+namespace NamingwayReborn;
 
 public sealed class Plugin : IDalamudPlugin
 {
@@ -21,7 +21,7 @@ public sealed class Plugin : IDalamudPlugin
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("NamingwayReborn");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
